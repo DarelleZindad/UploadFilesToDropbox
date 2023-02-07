a7838c0a43470b73d1b1a44a16dc00560bea8026
DarelleZindad <86950090+DarelleZindad@users.noreply.github.com>
2023-02-07 11:37:06 +0100

    Put Dropbox stuff in new class MyDropboxHelper.

--- Pages/Index.cshtml.cs
+++ Pages/Index.cshtml.cs
@@ -1,103 +1,32 @@
 using System;
 using System.Collections.Generic;
-using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
-using Dropbox.Api;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using UploadFilesToBB05022023.MyClasses;
 
 namespace UploadFilesToBB05022023.Pages
 {
     public class IndexModel : PageModel
     {
-        //temporary token, needs to be changed for every session. Will have to look into OAuth 2
-        static string token =
-           "sl.BYXKJ6J_drfFeqRb3wewomKkTCwcBtFwFp7ZspDK4HgM2ZW2NXcD5ycaWqVf9b19o528VmO-YHzK7m-ayexl0MAva2nZw9J3W9tSvIVqDkefvUWyvjzNmet-YjmoxAfhhZug61A";
+        readonly MyDropboxHelper dropboxHelper = new MyDropboxHelper();
 
-        Dropbox.Api.DropboxClient dbx;
-
-        private bool Connected = false;
-        private bool isConnecting = false;
-        private bool errorOccured = false;
-        string errorMessage;
-       
         public void OnGet()
         {
         }
 
         public IActionResult OnPost(IFormFile file)
         {           
-            if (!Connected)
+            if (!dropboxHelper.Connected)
             {
-                ConnectToDB();
+                dropboxHelper.ConnectToDB();
             }
             
-            return !errorOccured && UploadFile(file).Result
+            return !dropboxHelper.errorOccured && dropboxHelper.UploadFile(file).Result
              ? RedirectToPage("Success")
              : RedirectToPage("Error"); //I'd like to send errorMessage as well, but that'll have to wait.
-        }
-
-        async void ConnectToDB()
-        {
-            if (!Connected && !isConnecting)
-            {
-                isConnecting = true;
-                try
-                {
-                    dbx = new DropboxClient(token);
-                    var id = await dbx.Users.GetCurrentAccountAsync();
-                }
-                catch (Exception ex)
-                {
-                    errorOccured = true;
-                    errorMessage = ex.Message;
-                }
-
-                isConnecting = false;
-            }
-        }
-
-        async Task<bool> UploadFile(IFormFile file)
-        {
-            //file can't be directly loaded to DB bc they use different types of file
-            bool success = false;
-
-            if (file is null)
-            {
-                errorMessage = "no file chosen";
-            }
-            else
-            {
-                try
-                {
-                    var dbPath = "/UploadedFiles/" + file.FileName;
-                    var localPath = "UploadedFiles\\" + file.FileName;
-
-                    //copy file to folder
-                    await using var temoraryFile = new FileStream(localPath, FileMode.Create);
-                    await file.CopyToAsync(temoraryFile);
-                    temoraryFile.Close();
-
-                    //upload file to Dropbox
-                    var mem = new MemoryStream(System.IO.File.ReadAllBytes(localPath));
-                    await dbx.Files.UploadAsync(dbPath, body: mem);
-
-                    //delete file from folder
-                    if (System.IO.File.Exists(localPath))
-                    {
-                        System.IO.File.Delete(localPath);
-                    }
-                    success = true;
-                }
-                catch (Exception ex)
-                {
-                    errorMessage = ex.Message;
-                }
-            }
-
-            return success;
-        }       
+        }        
     }
 }
